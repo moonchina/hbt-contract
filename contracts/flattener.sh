fe9926c65deaa4a160f885a159d3a5626d3a85ee
houyi <zhangyueru@aliyun.com>
2020-10-27 15:55:31 +0800

    flattener

---
+++ contracts/flattener.sh
@@ -0,0 +1,4 @@
+truffle-flattener HBTLock.sol > output/HBTLock.sol 
+truffle-flattener HBTToken.sol > output/HBTToken.sol
+truffle-flattener MasterChef.sol > output/MasterChef.sol
+truffle-flattener Migrations.sol > output/Migrations.sol
